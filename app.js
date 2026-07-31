5f2b169b2a9f84d182ebb9654ae80a8d8e6cb6bc
jinski6146 <zijin6146@icloud.com>
2026-07-30 22:39:40 -0400

    Test app.js

--- app.js
+++ app.js
@@ -8,13 +8,5 @@ app.get('/', (req, res)=>{
     res.sendFile("index.html", {root: "views"})
 })
 
-
-
 // start server
-// app.listen(3000, "0.0.0.0");
-
-app.listen(0, () => {
-
-    console.log(app.address());
-
-});
+app.listen(3000, "0.0.0.0");
